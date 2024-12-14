63050a6fd3d2a1547b6638710ba69bf4dd4f58bb
brewermawa <brewermawa@gmail.com>
2024-12-14 10:28:22 -0600

    Working with stock record to add discounted price

--- apps/partner/models.py
+++ apps/partner/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from oscar.apps.partner.abstract_models import AbstractStockRecord
 
 class StockRecord(AbstractStockRecord):
-    pass
+    discounted_price = models.DecimalField(decimal_places=2, max_digits=12, blank=True, null=True)
 
 from oscar.apps.partner.models import * 
  
