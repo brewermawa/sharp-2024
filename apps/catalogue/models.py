3eb88bb33a397d1f0e512042560c546db0d80a1c
brewermawa <brewermawa@gmail.com>
2025-02-06 09:18:42 -0600

    Working with index.html

--- apps/catalogue/models.py
+++ apps/catalogue/models.py
@@ -5,5 +5,8 @@ from oscar.apps.catalogue.abstract_models import AbstractProduct
 class Product(AbstractProduct):
     short_description = models.TextField(max_length=255, blank=True)
 
+    def calculate_non_rating(self):
+        return 5 - self.calculate_rating()
+
 from oscar.apps.catalogue.models import * 
  
